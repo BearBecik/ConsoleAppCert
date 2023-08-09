5a434ec6ae4c8463fdbb61e3f7694113912747a5
Arek B <Arkadiusz.Bett@gmail.com>
2023-08-09 11:48:04 +0200

    Dzień 21 - praca ko

---
+++ ConsoleAppCert/IStudent.cs
@@ -0,0 +1,17 @@
+﻿using static ConsoleAppCert.StudentBase;
+
+namespace ConsoleAppCert
+{
+    public interface IStudent
+    {
+        string Name { get; }
+        string Surname { get; }
+        int Age { get; }
+
+        event GradeAddedDelegate GradeAdded;
+        void AddGrade(double grade);
+        void AddGrade(string grade);
+        void PartialResults();
+        Statistics GetStatistics();
+    }
+}
